643801bd49e4110042c561763ad1fb7b644d3631
MNad48 <nadeemmalikali@hotmail.com>
2021-09-14 07:07:12 +0500

    Committing changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title = "Angular Task Manager"
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleAddTask() {
+    console.log('Toggle');
   }
 
 }
---
+++ src/app/task.mock.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASK:Task[] = [
+    {
+        id:1,
+        title: "First Task",
+        date:"1 May at 8:30 AM",
+        reminder:true
+    },
+    {
+        id:2,
+        title: "Second Task",
+        date:"1 May at 9:30 AM",
+        reminder:true
+    },
+    {
+        id:3,
+        title: "Third Task",
+        date:"1 May at 10:30 aM",
+        reminder:true
+    },
+    {
+        id:4,
+        title: "Fourth Task",
+        date:"1 May at 11:30 AM",
+        reminder:true
+    }
+]
